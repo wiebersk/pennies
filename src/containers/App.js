5d356304103168dc98b2306c7f1584370bde2714
Kyle Wiebers <kyle@wiebe.rs>
2017-01-01 20:40:05 -0600

    Update App to load budgets when component mounts

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,19 +4,26 @@ import api from '../utils/Api.js'
 import {RETRIEVE_BUDGETS_REQUESTED, CREATE_BUDGET_REQUESTED} from '../actions/budget.js'
 import {connect} from 'react-redux';
 
-const App = ({onBudgetClick, onBudgetCreateClick}) => {
+class App extends React.Component {
+
+  componentDidMount() {
+    this.props.loadBudgets();
+  }
+
+  render() {
     return (
       <div>
         <h1>Test App</h1>
-        <Button onClick={onBudgetClick}>Budgets</Button>
-        <Button onClick={onBudgetCreateClick}>Create Budget</Button>
+        <Button onClick={this.props.loadBudgets}>Budgets</Button>
+        <Button onClick={this.props.onBudgetCreateClick}>Create Budget</Button>
       </div>
     );
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onBudgetClick: () => {
+    loadBudgets: () => {
       dispatch({type: RETRIEVE_BUDGETS_REQUESTED})
     },
     onBudgetCreateClick: () => {
